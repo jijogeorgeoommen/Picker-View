fb1f63b5e4a89618ec7862264a9b08f82c2a283a
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-05-30 10:27:57 +0530

    Initial Commit

---
+++ Picker View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picker View
+//
+//  Created by JIJO G OOMMEN on 30/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Picker ViewTests/Picker_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Picker_ViewTests.swift
+//  Picker ViewTests
+//
+//  Created by JIJO G OOMMEN on 30/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import XCTest
+@testable import Picker_View
+
+class Picker_ViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
