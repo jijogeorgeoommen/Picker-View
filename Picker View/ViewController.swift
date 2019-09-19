fee1aed16aff6c39d30a1654cee9b31b27e9bb75
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-09-19 08:15:57 +0530

    initial

--- Picker View/ViewController.swift
+++ Picker View/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    
+    var a = ["item 1","item 2","item 3","item 4","item 5"]
+    var b = ["1","2","3","4","5"]
 
+    @IBOutlet weak var tExt: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        //self.tExt.inputView = pickerView
     }
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return a.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return a[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tExt.text = "\(a[row])"
+    }
 
 }
 
